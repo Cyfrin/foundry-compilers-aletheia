5487890a00ef3b82014d4998d80fee10859bfd89
TilakMaddy <tilak.madichetti@gmail.com>
2025-03-09 00:15:29 +0530

    forge soldeer tests

--- tests/config.rs
+++ tests/config.rs
@@ -16,3 +16,20 @@ mod foundry_basic {
         assert_eq!(raw_config.remappings.len(), 1);
     }
 }
+
+mod foundry_soldeer_basic {
+
+    use pretty_assertions::assert_eq;
+    use std::ffi::OsStr;
+
+    use super::*;
+
+    const ROOT: &'static str = "test-configs/foundry-soldeer-basic";
+
+    #[test]
+    fn loads_config_currently() {
+        let raw_config = Config::load_with_root(ROOT).unwrap(); /*.sanitized()*/
+        assert_eq!(raw_config.src.as_os_str(), OsStr::new("src"));
+        assert_eq!(raw_config.remappings.len(), 2);
+    }
+}
