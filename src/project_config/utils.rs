1a8a7177fbb0e8f23500359dce8f95f27b3fe0b1
TilakMaddy <tilak.madichetti@gmail.com>
2025-03-16 14:10:17 +0530

    perf

--- src/project_config/utils.rs
+++ src/project_config/utils.rs
@@ -2,16 +2,20 @@ use super::ProjectConfigInput;
 use std::path::{Path, PathBuf};
 use walkdir::WalkDir;
 
-pub fn source_files_iter(
-    root: &Path,
-    config: &ProjectConfigInput,
-) -> impl Iterator<Item = PathBuf> {
-    WalkDir::new(root)
+pub fn source_files_iter(src: &Path, config: &ProjectConfigInput) -> impl Iterator<Item = PathBuf> {
+    WalkDir::new(src)
         .follow_links(true)
         .into_iter()
+        .filter_entry(|e| {
+            // skip from foundry.toml config
+            config.skip.iter().all(|skipper| !skipper.is_match(e.path())) &&
+
+            // don't descend into directories that are meant to be excluded
+            config.exclude_starting.iter().all(|exclude| !e.path().starts_with(exclude))
+        })
         .filter_map(Result::ok)
         .filter(|e| e.file_type().is_file())
         .filter(|e| e.path().extension().map(|ext| ext == "sol").unwrap_or_default())
-        .filter(|e| config.is_included(e.path()))
+        .filter(|e| config.is_included(e.path())) // Final filter
         .map(|e| e.path().into())
 }
